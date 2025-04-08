6fefbe9567df4358d7ed50055d8bff39fc892457
Anil Kunda <anilkunda701@gmail.com>
2025-04-08 22:45:11 +0530

    implement game restart functionality

--- 04-DOM-and-Events/03-Pig-Game/script.js
+++ 04-DOM-and-Events/03-Pig-Game/script.js
@@ -14,15 +14,27 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+// Function to initialize variables
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+};
+init();
 
 // Function to switch player
 const switchPlayer = function () {
@@ -85,3 +97,6 @@ btnHold.addEventListener("click", function () {
     }
   }
 });
+
+// Restart game
+btnNew.addEventListener("click", init);
