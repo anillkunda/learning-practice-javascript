e2d31b4cf285fd4e369d8531b932200dd060a0eb
Anil Kunda <anilkunda701@gmail.com>
2025-05-07 00:09:46 +0530

    refactoring our code: the DRY principle

--- 04-DOM-and-Events/01-Guessing-Numbar/script.js
+++ 04-DOM-and-Events/01-Guessing-Numbar/script.js
@@ -2,61 +2,55 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-let highScore = 0;
+let highscore = 0;
 
-document.querySelector('.number').textContent = secretNumber;
+// TODO: ----- Display message function
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
-  //  When there is no input
+  // TODO: ----- When there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔️ No Number!';
+    displayMessage('⛔️ ON Number!');
 
-    // When player wins
+    // TODO: ----- When player wins
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉 Correct  Number!';
+    displayMessage('🎉 Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
 
     // Manipulating CSS Style
     document.querySelector('body').style.background = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
-    if (score > highScore) {
-      highScore = score;
-      document.querySelector('.highScore').textContent = highScore;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
     }
 
-    //  When guess is too high
-  } else if (guess > secretNumber) {
+    // TODO: ----- When guess is wrong
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Too High!';
+      displayMessage(guess > secretNumber ? '📈 Too High!' : ' 📉 Too Low!');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = '💥 You lost the game!';
-      document.querySelector('.score').textContent = 0;
-    }
-
-    //  When guess is too low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📉 Too Low!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '💥 You lost the game!';
+      displayMessage('💥 You lost the game!');
       document.querySelector('.score').textContent = 0;
     }
   }
 });
 
-// Game reset functionality.
+// TODO: ----- Game reset functionality.
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-  document.querySelector('.message').textContent = 'Start guessing...';
+  // document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
