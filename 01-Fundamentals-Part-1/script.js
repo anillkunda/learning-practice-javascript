7ea6c6aa6c19ecf75edfd7667bbc48d0402daf90
Anil Kunda <anilkunda701@gmail.com>
2025-06-07 21:59:53 +0530

    primitive data types

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -38,3 +38,47 @@ console.log(currentJob);
    multi line
    Comment
 */
+
+// ------- ** Data Types
+
+// --- JavaScript Primitive Data Types [String, Number, Boolean, Null, Undefined, Symbol]
+
+// 01. "String" - [ Single Quotes (''), Double Quotes(""), & Backticks (``)]
+
+const customerName = 'Manoj';
+const purchaseAmount = 123.45;
+
+const greetingMessage = `Hello, ${customerName}! Thank you for your purchase of $${purchaseAmount}`;
+console.log(greetingMessage, typeof greetingMessage); // string
+
+// 02. Number - represent quantitative data like prices, quantities, and ratings.
+
+const productPrice = 899.99; // Price in dollars
+const stockQuantity = 20; // Quantity
+const productRating = 4.8; // rating (out of 5 stars)
+
+console.log(productRating, typeof productRating); // number
+
+// 03. Booleans - This type is commonly used to store yes/no values
+
+// true --- means "Yes, correct" 1
+// false --- means "no, incorrect" 0
+
+// Is the product on sale?
+const isOnSale = true;
+
+// Is the product available for purchase?
+const isAvailable = false;
+
+console.log(isAvailable, typeof isAvailable); // boolean
+
+// 04. Null - is just a special value which represents “nothing”, “empty” or “value unknown”.
+
+const discountCode = null; // No discount code applied
+console.log(discountCode, typeof discountCode); // object
+
+// 05. Undefined - A variable that has not been assigned a value is undefined.
+
+let specialOffer;
+// let specialOffer = undefined
+console.log(specialOffer, typeof specialOffer); // undefined
